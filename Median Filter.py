5266c4d82ec8a6ebd196f1ef920d8b03a1b344bd
ahmed77mostafa <ahmedmostafa.0522030@gmail.com>
2024-12-12 09:07:06 +0200

    Change kernel size Commit

--- Median Filter.py
+++ Median Filter.py
@@ -1,7 +1,7 @@
 import cv2
 
 image = cv2.imread('Images/OIP.jpg')
-median_image = cv2.medianBlur(image, ksize = 7)
+median_image = cv2.medianBlur(image, ksize = 5)
 
 cv2.imshow('Original Image',image)
 cv2.imshow('Median Filter Image', median_image)
