add09cd8976f9081a7162b8558007d78deb939f2
ahmed77mostafa <ahmedmostafa.0522030@gmail.com>
2024-12-12 10:50:56 +0200

    Imwrite Median Filter photo

--- Median Filter.py
+++ Median Filter.py
@@ -6,5 +6,7 @@ median_image = cv2.medianBlur(image, ksize = 5)
 cv2.imshow('Original Image',image)
 cv2.imshow('Median Filter Image', median_image)
 
+cv2.imwrite('Images/Median Filter Image.jpg', image)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
