e52806759b527f715059239cc4e10cabd8252d12
ahmed77mostafa <ahmedmostafa.0522030@gmail.com>
2024-12-15 09:21:37 +0200

    Update Image Rotation.py

--- Image Rotation.py
+++ Image Rotation.py
@@ -2,10 +2,12 @@ import cv2
 import imutils
 
 image = cv2.imread('Images/BASTANUK.jpg')
-rotated_image = imutils.rotate(image, 45)
+rotated_image = cv2.rotate(image,cv2.ROTATE_90_COUNTERCLOCKWISE)
+rotated_image2 = imutils.rotate(image, 45)
 
 cv2.imshow('Original Image', image)
 cv2.imshow('Rotated Image', rotated_image)
+cv2.imshow('Rotated Image 2', rotated_image2)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
